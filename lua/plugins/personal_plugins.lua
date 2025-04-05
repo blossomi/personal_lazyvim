4702f40526a9f87467af478412e6451943a7f72c
harry <1171053130@qq.com>
2025-04-05 22:55:48 +0800

    remove neotree from config file

--- lua/plugins/personal_plugins.lua
+++ lua/plugins/personal_plugins.lua
@@ -58,24 +58,4 @@ return {
       "stevearc/overseer.nvim",
     },
   },
-
-  ---------------------------------------- neo tree------------------------------
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    ---@module "neo-tree"
-    ---@type neotree.Config?
-    opts = {
-      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
-      popup_border_style = "rounded", -- "double", "rounded", "solid", "none"
-      enable_git_status = true, -- false by default
-    },
-  },
 }
