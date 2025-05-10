5602376ab215d21416e35434f150c6e28b315306
harry <1171053130@qq.com>
2025-05-10 21:30:26 +0800

    add gitsigns
    add gitsigns

--- lua/plugins/personal.lua
+++ lua/plugins/personal.lua
@@ -9,8 +9,9 @@ return {
       "jose-elias-alvarez/typescript.nvim",
       init = function()
         require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+                    -- stylua: ignore
+                    vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports",
+                        { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
         end)
       end,
@@ -68,7 +69,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
 
     settings = {
-      save_on_toggle = ture,
+      save_on_toggle = true,
       sync_on_ui_close = true,
       key = function()
         return vim.loop.cwd()
@@ -98,7 +99,7 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-neotest/neotest",
-      "nvim-telescope/telescope.nvim",
+      -- "nvim-telescope/telescope.nvim",
       "stevearc/overseer.nvim",
     },
   },
@@ -109,7 +110,6 @@ return {
     config = function()
       require("neotest").setup({
         adapters = {
-          require("neotest-hardhat"),
           require("rustaceanvim.neotest"),
         },
       })
@@ -150,7 +150,6 @@ return {
   {
     "mrcjkb/rustaceanvim",
     version = "^6", -- Recommended
-    lazy = false, -- This plugin is already lazy
   },
 
   {
@@ -169,32 +168,54 @@ return {
 
   ----------------------------------------- conform -------------------------------------
 
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-      },
-      formatters = {
-        prettier = {
-          prepend_args = {
-            "--tab-width",
-            "4",
-            "--print-width",
-            "120",
-          },
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+    },
+    formatters = {
+      prettier = {
+        prepend_args = {
+          "--tab-width",
+          "4",
+          "--print-width",
+          "120",
         },
       },
     },
   },
+  ----------------------------------------- gitsigns -----------------------------------------
+  -- ~/.config/nvim/lua/plugins/gitsigns-custom.lua
+  {
+    "lewis6991/gitsigns.nvim", -- 确保插件名与 LazyVim 中定义的一致
+    opts = function(_, opts) -- 使用 opts 函数来合并或覆盖默认配置
+      -- opts 参数包含了 LazyVim 为 gitsigns 设置的默认选项
+      -- 你可以在这里修改或添加新的选项
+
+      -- 启用或自定义当前行 blame 信息
+      opts.current_line_blame = true
+      opts.current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 显示在行尾
+        delay = 500, -- 延迟 500ms 显示
+        -- ignore_whitespace = false,
+      }
+      -- 自定义 blame 信息的显示格式
+      opts.current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> • <summary>"
+      -- 你也可以设置高亮
+      opts.current_line_blame_formatter_hl = "Todo"
+
+      return opts -- 必须返回修改后的 opts 表
+    end,
+  },
   ----------------------------------------- augment -------------------------------------------
-  { "augmentcode/augment.vim" },
+  -- { "augmentcode/augment.vim" },
 }
